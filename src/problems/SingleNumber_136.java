c9196c91c19dd0dcc800e93d86b90e9f44dd71d6
zyh-Win <cmxx@outlook.com>
2016-11-29 01:44:55 -0800

    Added #136 Single Number

---
+++ src/problems/SingleNumber_136.java
@@ -0,0 +1,22 @@
+package problems;
+
+public class SingleNumber_136 {
+    // # 136 Single Number
+    // Given an array of integers, every element appears twice except for one. Find that single one.
+    // Status: Unfinished.
+    
+    public int singleNumber(int[] nums) {
+        for (int i=0; i<nums.length; i++){
+            
+        }
+        return 0;
+    }
+    
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        SingleNumber_136 t = new SingleNumber_136();
+        int[] test1  ={1,5,8,2,4,8,1,4,5};  // Find 
+        System.out.println(t.singleNumber(test1));
+    }
+
+}
